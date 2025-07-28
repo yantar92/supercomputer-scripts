477eeab53f5b46541a611a8f23b9d9074788055f
Ihor Radchenko <yantar92@posteo.net>
2025-07-28 14:14:25 +0200

    log

--- 2025.graphite-expanded.CE/atat-voltage.py
+++ 2025.graphite-expanded.CE/atat-voltage.py
@@ -105,7 +105,7 @@ def _get_true_gs(gs_data, fit_data, extra_paths, ion, base_N):
             continue
         atat_index = gs_data.loc[mask, 'index'].min()
         atat_energy = _get_normalized_energy(Path(str(atat_index)), ion, base_N)
-        print(f"ATAT energy = {atat_energy} ({atat_index}); new = {energy} ({index})")
+        print(f"c = {c}; ATAT energy = {atat_energy} ({atat_index}); new = {energy} ({index})")
         if energy < atat_energy:
             print(f"Found energy below ATAT hull!: c={c}, energy={energy} ({atat_index}) < {atat_energy} ({index})")
     return gs_concentrations, gs_energies
