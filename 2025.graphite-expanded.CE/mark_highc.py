8c0e0c2e44b5f169d51a54557e568c2de5b8cf45
Ihor Radchenko <plgyantar92@login01.helios.cyfronet.pl>
2025-10-16 14:56:07 +0200

    Print number of high concentrations; change threshold

--- 2025.graphite-expanded.CE/mark_highc.py
+++ 2025.graphite-expanded.CE/mark_highc.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from pathlib import Path
 
-max_c = 0.19 # 0.33 will still yield large sublattice deviations
-
-for d in Path().iterdir():
+max_c = 0.34 # 0.33 will still yield large sublattice deviations
+max_c = 0.67 # 0.25 in PhaseDiagram convention: LiC3 = Li0.67C2. For manual convex hull
+counter = 0
+def process_dir(d):
     if d.is_dir() and (d / 'str.out').is_file():
         with open(d / 'str.out', 'r', encoding='utf-8') as f:
             n_Na = 0
@@ -11,10 +12,21 @@ for d in Path().iterdir():
             for line in f:
                 if 'Vac' in line:
                     n_Vac += 1
-                elif 'Na' in line:
+                elif 'Na' in line or 'Li' in line:
                     n_Na += 1
-            concentration = n_Na / (n_Na + n_Vac)
-            if concentration > max_c and not (d / 'error').is_file():
+            concentration = n_Na / (n_Na + n_Vac) if n_Na + n_Vac > 0 else None
+            if concentration is not None and concentration > max_c and not (d / 'error').is_file():
+                global counter
+                counter += 1
                 print(f'{d}: {concentration} marking with error')
                 (d / 'error').touch()
-                (d / 'marked_error').touch()
+                (d / 'error_highc').touch()
+extra_dirs = [Path()]
+for d in Path().iterdir():
+    if d.is_dir() and not (d / 'str.out').is_file():
+        extra_dirs.append(d)
+print(extra_dirs)
+for root in extra_dirs:
+    for d in root.iterdir():
+        process_dir(d)
+print(f"Total number of concentrations above {max_c} is {counter}")
