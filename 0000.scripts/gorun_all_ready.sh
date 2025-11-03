6c38b350775ebd133e38aca8ee1248b749c12904
Ihor Radchenko <yantar92@posteo.net>
2025-11-03 15:34:45 +0100

    gorun_all_ready.sh: Limit max jobs

--- 0000.scripts/gorun_all_ready.sh
+++ 0000.scripts/gorun_all_ready.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
+
+get_n_jobs () {
+    squeue -u $USER -o %Z | tail -n +2 | wc -l
+}
+
+[ -z "$1" ] && max_jobs="100" || max_jobs="$1"
+echo "Limiting max jobs to $max_jobs"
 find . -iname "gorun_ready" | while read x; do
+    while [ get_n_jobs -ge $max_jobs ]; do
+	echo "Waiting for submitted jobs to finish"
+        sleep 5
+    done
     (cd $(dirname $x);
      echo $x
      sbatch sub && rm gorun_ready)
