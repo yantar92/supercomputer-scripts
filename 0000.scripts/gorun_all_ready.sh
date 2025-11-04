d0d10612a8da076e2b5b81a1bfe5a3a19331d9ee
Ihor Radchenko <yantar92@posteo.net>
2025-11-04 08:51:16 +0100

    gorun_all_ready.sh: Be less verbose while waiting

--- 0000.scripts/gorun_all_ready.sh
+++ 0000.scripts/gorun_all_ready.sh
@@ -7,8 +7,8 @@ get_n_jobs () {
 [ -z "$1" ] && max_jobs="100" || max_jobs="$1"
 echo "Limiting max jobs to $max_jobs"
 find . -iname "gorun_ready" | while read x; do
+    [ "$(get_n_jobs)" -ge "$max_jobs" ] && echo "Waiting for submitted jobs to finish"
     while [ "$(get_n_jobs)" -ge "$max_jobs" ]; do
-	echo "Waiting for submitted jobs to finish"
         sleep 5
     done
     (cd $(dirname $x);
