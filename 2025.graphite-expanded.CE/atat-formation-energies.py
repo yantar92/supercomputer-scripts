4c17b7e22b0e22e3524262c7e569597dfa024171
Ihor Radchenko <yantar92@posteo.net>
2025-11-04 15:23:58 +0100

    fix

--- 2025.graphite-expanded.CE/atat-formation-energies.py
+++ 2025.graphite-expanded.CE/atat-formation-energies.py
@@ -143,7 +143,7 @@ def plot_custom_phase_diagram(
         'Energy': entry.energy_per_atom,
         'Concentration': coords[0],
         'Formation Energy (meV/atom)': coords[1] * energy_mult,
-        'Formula': f"{ion_element}C{int((1 - coords[0])/coords[0])}"
+        'Formula': "C" if np.isclose(coords[0], 0) else f"{ion_element}C{int((1 - coords[0])/coords[0])}"
     } for entry, coords in unstable_entries.items() if phd.get_e_above_hull(entry) is not None and phd.get_e_above_hull(entry) < show_unstable]
     # Now, append all stable points to the same unstable_data
     stable_data = [{
@@ -151,7 +151,7 @@ def plot_custom_phase_diagram(
         'Energy': entry.energy_per_atom,
         'Concentration': coords[0],
         'Formation Energy (meV/atom)': coords[1] * energy_mult,
-        'Formula': f"{ion_element}C{int((1 - coords[0])/coords[0])}"
+        'Formula': "C" if np.isclose(coords[0], 0) else f"{ion_element}C{int((1 - coords[0])/coords[0])}"
     } for coords, entry in stable_entries.items()]
     data.extend(stable_data)
 
