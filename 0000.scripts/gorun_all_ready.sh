6b68b2ec44b64e07ed198b0fd038baf56ffc7730
Ihor Radchenko <yantar92@posteo.net>
2025-11-04 09:03:32 +0100

    gorun_all_ready.sh: Run in sorted order

--- 0000.scripts/gorun_all_ready.sh
+++ 0000.scripts/gorun_all_ready.sh
@@ -6,7 +6,7 @@ get_n_jobs () {
 
 [ -z "$1" ] && max_jobs="100" || max_jobs="$1"
 echo "Limiting max jobs to $max_jobs"
-find . -iname "gorun_ready" | while read x; do
+find . -iname "gorun_ready" | sort | while read x; do
     [ "$(get_n_jobs)" -ge "$max_jobs" ] && echo "Waiting for submitted jobs to finish"
     while [ "$(get_n_jobs)" -ge "$max_jobs" ]; do
         sleep 5
