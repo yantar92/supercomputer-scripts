95fd8b33b5b7f42f8d87cdf4322244f51d3fadde
Ihor Radchenko <yantar92@posteo.net>
2026-03-03 13:48:45 +0100

    atat-formation-energies.py: Now, back to formula for single elem

--- 2025.graphite-expanded.CE/atat-formation-energies.py
+++ 2025.graphite-expanded.CE/atat-formation-energies.py
@@ -578,18 +578,18 @@ def main():
                     temperature
                 )
             )
-            from scipy.interpolate import interp1d
-            if entry.composition.reduced_composition.num_atoms == 1:
-                print(entry.composition.get_atomic_fraction(args.ion), sisso_corr/entry.composition.num_atoms)
-                elems = entry.composition.get_el_amt_dict()
-                sum_g_i = 0
-                for elem, amt in elems.items():
-                    g_interp = interp1d(
-                        [float(t) for t in G_ELEMS],
-                        [g_dict[elem] for g_dict in G_ELEMS.values()],
-                    )
-                    sum_g_i += amt * g_interp(temperature)
-                sisso_corr = sum_g_i
+            # from scipy.interpolate import interp1d
+            # if entry.composition.reduced_composition.num_atoms == 1:
+            #     print(entry.composition.get_atomic_fraction(args.ion), sisso_corr/entry.composition.num_atoms)
+            #     elems = entry.composition.get_el_amt_dict()
+            #     sum_g_i = 0
+            #     for elem, amt in elems.items():
+            #         g_interp = interp1d(
+            #             [float(t) for t in G_ELEMS],
+            #             [g_dict[elem] for g_dict in G_ELEMS.values()],
+            #         )
+            #         sum_g_i += amt * g_interp(temperature)
+            #     sisso_corr = sum_g_i
 
             print(entry.composition.get_atomic_fraction(args.ion), sisso_corr/entry.composition.num_atoms)
             entry.correction += sisso_corr
